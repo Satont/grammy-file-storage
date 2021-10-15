2b34e643438008c325259cf6aa4ff85166c40a64
Satont <satontworldwide@gmail.com>
2021-10-15 16:39:40 +0300

    Bump deno std

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,6 +1,6 @@
 export type { StorageAdapter } from 'https://deno.land/x/grammy@v1.3.3/mod.ts'
-import { exists, existsSync, ensureDirSync } from 'https://deno.land/std@0.77.0/fs/mod.ts'
-import { resolve } from 'https://deno.land/std@0.110.0/path/mod.ts'
+import { exists, existsSync, ensureDirSync } from 'https://deno.land/std@0.111.0/fs/mod.ts'
+import { resolve } from 'https://deno.land/std@0.111.0/path/mod.ts'
 
 export const fs = {
   readFile: Deno.readTextFile,
