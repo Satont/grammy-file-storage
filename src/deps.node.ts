b9a0d210c0ec9f154cfb084c5c2b3aec462f2158
Satont <satontworldwide@gmail.com>
2021-10-20 04:51:13 +0300

    Add missed functions to node tests

--- src/deps.node.ts
+++ src/deps.node.ts
@@ -1,16 +1,31 @@
 export type { StorageAdapter } from 'grammy'
-import { readFile, writeFile, rm } from 'fs/promises'
+import { readFile, writeFile, rm, stat, mkdir } from 'fs/promises'
 import { existsSync, mkdirSync } from 'fs'
 import { resolve } from 'path'
 
+interface SystemError {
+  code?: string;
+  errno?: number;
+  syscall?: string;
+  path?: string;
+}
+
 export const fs = {
   readFile: (path: string) => readFile(path, { encoding: 'utf-8' }),
-  writeFile,
+  writeFile: (path: string, data: string) => writeFile(path, data, { encoding: 'utf-8' }),
   existsSync,
+  ensureDir: async (path: string) => {
+    try {
+      await stat(path)
+    } catch (e) {
+      if ((e as SystemError).code === 'ENOENT') {
+        await mkdir(path, { recursive: true })
+      } else throw e
+    }
+  },
   ensureDirSync: (path: string) => {
-    console.log(path, existsSync(path))
     if (!existsSync(path)) {
-      mkdirSync(path)
+      mkdirSync(path, { recursive: true })
     }
   },
   remove: (path: string) => rm(path, { recursive: true })
