598c92f72c5b83ec555831e1e45c349e507fb36b
Satont <satontworldwide@gmail.com>
2021-11-17 01:31:29 +0300

    Add serializer and deserializer options to constuctor
    fixes #3

--- src/mod.ts
+++ src/mod.ts
@@ -1,46 +1,73 @@
-import { StorageAdapter, fs, path, cwd } from './deps.deno.ts'
+import { StorageAdapter, fs, path, cwd } from './deps.deno.ts';
+
+type ConstructorOptions = {
+  dirName?: string;
+  serializer?: (input: any) => string;
+  deserializer?: (input: string) => unknown;
+};
 
 export class FileAdapter<T> implements StorageAdapter<T> {
-  private folderPath: string
+  private folderPath: string;
+  serializer: ConstructorOptions['serializer'];
+  deserializer: ConstructorOptions['deserializer'];
 
-  constructor({ dirName }: { dirName: string }) {
-    this.folderPath = path.resolve(cwd(), dirName)
-    fs.ensureDirSync(this.folderPath)
+  /**
+   * @constructor
+   * @param {opts} options options
+   * @param {opts.dirname} options.ttl - name of directory where files should be stored.
+   * @param {opts.serializer} options.serializer
+   * serializer of file. Default `JSON.stringify(input, null, '\t')`.
+   *
+   * @param {opts.deserializer} options.deserializer
+   * deserializer of file. Default `JSON.parse(input)`.
+   */
+  constructor(opts: ConstructorOptions = {}) {
+    this.folderPath = path.resolve(cwd(), opts?.dirName ?? 'sesions');
+
+    if (!opts.serializer) {
+      this.serializer = (input) => JSON.stringify(input, null, '\t');
+    }
+
+    if (!opts.deserializer) {
+      this.deserializer = (input: string) => JSON.parse(input);
+    }
+
+    fs.ensureDirSync(this.folderPath);
   }
 
   private resolveSessionPath(key: string) {
-    const subFolder = key.substr(-2)
-    return path.resolve(this.folderPath, subFolder, `${key}.json`)
+    const subFolder = key.substr(-2);
+    return path.resolve(this.folderPath, subFolder, `${key}.json`);
   }
 
   private async findSessionFile(key: string) {
     try {
-      return await fs.readFile(this.resolveSessionPath(key))
+      return await fs.readFile(this.resolveSessionPath(key));
     } catch (error) {
-      return null
+      return null;
     }
   }
 
   async read(key: string) {
-    const file = await this.findSessionFile(key)
+    const file = await this.findSessionFile(key);
 
     if (!file) {
-      return undefined
+      return undefined;
     }
 
-    return JSON.parse(file) as T
+    return this.deserializer!(file) as T;
   }
 
   async write(key: string, value: T) {
-    const fullPath = this.resolveSessionPath(key)
-    const fileName = `${key}.json`
-    const folderPath = fullPath.substring(0, fullPath.length - fileName.length)
+    const fullPath = this.resolveSessionPath(key);
+    const fileName = `${key}.json`;
+    const folderPath = fullPath.substring(0, fullPath.length - fileName.length);
 
-    await fs.ensureDir(folderPath)
-    await fs.writeFile(fullPath, JSON.stringify(value))
+    await fs.ensureDir(folderPath);
+    await fs.writeFile(fullPath, this.serializer!(value));
   }
 
   async delete(key: string) {
-    await fs.remove(this.resolveSessionPath(key))
+    await fs.remove(this.resolveSessionPath(key));
   }
 }
