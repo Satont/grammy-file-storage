ec7fd394a81ccf5f5e6f0abcae11b49de80cff3a
GitHub <noreply@github.com>
2021-12-11 00:06:23 +0000

    Update dependencies

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,6 +1,6 @@
-export type { StorageAdapter } from 'https://deno.land/x/grammy@v1.5.2/mod.ts'
-import { exists, existsSync } from 'https://deno.land/std@0.111.0/fs/mod.ts'
-import { resolve } from 'https://deno.land/std@0.111.0/path/mod.ts'
+export type { StorageAdapter } from 'https://deno.land/x/grammy@v1.5.3/mod.ts'
+import { exists, existsSync } from 'https://deno.land/std@0.117.0/fs/mod.ts'
+import { resolve } from 'https://deno.land/std@0.117.0/path/mod.ts'
 
 export const fs = {
   readFile: Deno.readTextFile,
